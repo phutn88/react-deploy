977ccdc917282cccf1e0031d7afd55ec04e80a28
phnguye <phnguye@phnguyes-MBP.attlocal.net>
2020-06-16 22:40:00 -0700

    Updates

---
+++ precache-manifest.bf827d11e120d59422465d4fa9f91926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a33c0ab4d409ec98c658df225d3e069",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "40dac5208106ce034f89",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40dac5208106ce034f89",
+    "url": "/react-deploy/static/js/main.b2792aba.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  }
+]);
